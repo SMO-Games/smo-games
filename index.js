6e575360b581ae4d08f925b5c1aed40eb2d41813
SMO-Games <268230670+SMO-Games@users.noreply.github.com>
2026-03-20 15:55:09 +0000

    fix any% pb tooltip

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ function addRow(rowNum){
     let categoryTooltips = ["The runner's username on Speedrun.com.",
                             "The runner's nationality set on Speedrun.com. Turns yellow if in the correct continent.",
                             "The console used in the runner's Any% 1P PB.",
-                            "The runner's Any% 1P PB. Turns yellow if within 1 minute.\n⬆️ = Faster\n⬇️ = Slower",
+                            "The runner's Any% 1P PB. Turns yellow if within 1 minute.\n⬆️ = Slower\n⬇️ = Faster",
                             `The date of the runner's most recently submitted run on either official SMO leaderboard.\nTurns yellow if within ${mostRecentRange} days.\n⬆️ = More recent\n⬇️ = Less recent`,
                             `The runner's current best Main Leaderboard placement in any category.\nTurns yellow if within ${bestPlacementRange}.\n⬆️ = Better placement\n⬇️ = Worse placement`,
                             `The runner's current best CE Leaderboard placement in any category.\nTurns yellow if within ${bestPlacementRange}.\n⬆️ = Better placement\n⬇️ = Worse placement`
