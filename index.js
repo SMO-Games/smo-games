8dd4e56b9860aa6bd5d12586b2889768eb037814
SMO-Games <268230670+SMO-Games@users.noreply.github.com>
2026-03-21 23:48:27 +0000

    add sand kingdom background

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const bgImgs = [
     "background1.jpg", "background2.jpg", "background3.jpg", 
     "background4.jpg", "background5.jpg", "background6.jpg", 
     "background7.jpg", "background8.jpg", "background9.jpg",
-    "background10.jpg"
+    "background10.jpg", "background11.jpg"
 ]
 // range to be within to turn yellow
 const mostRecentRange = 60;
