0fc9d5ba8689df0daf4812985a26eb0534971f13
SMO-Games <268230670+SMO-Games@users.noreply.github.com>
2026-03-31 20:35:05 +0100

    add space after kingdom name

--- trickjump-generator/generator.js
+++ trickjump-generator/generator.js
@@ -98,7 +98,7 @@ function generateTrickjump(){
 
     // 1 in 8 chance of having a kingdom
     if(Math.floor(Math.random()*8 + 1) === 1){
-        trickjumpName += kingdom;
+        trickjumpName += `${kingdom} `;
     }
 
     trickjumpName += `${adjective} `;
